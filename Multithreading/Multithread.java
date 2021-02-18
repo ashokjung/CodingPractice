600aa5b85e8c3a3fcbfd0fe80c173b568f493c7e
ashokjung <ashokjung@gmail.com>
2021-02-18 18:22:53 +0530

    Multi-threading

--- Multithreading/Multithread.java
+++ Multithreading/Multithread.java
@@ -1,3 +1,4 @@
+package Multithreading;
 /*
 * Multithreading is a Java feature that allows concurrent execution of two or more parts of a program for maximum utilization of CPU. 
 * Each part of such program is called a thread. So, threads are light-weight processes within a process.
