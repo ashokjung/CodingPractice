9421759981440e5688d44cf32df42cd5820c442f
ashokjung <ashokjung@gmail.com>
2021-02-20 15:34:42 +0530

    updated

--- Multithreading/FizzBuzz.java
+++ Multithreading/FizzBuzz.java
@@ -4,10 +4,11 @@ public class FizzBuzz{
 
     public static void main(String[] args)
     {
-        for(int i=0; i<100; i++){
+        for(int i=0; i<110; i++){
             String output= "";
             if(i%3==0) { output +="Fizz";}
             if(i%5==0){output +="Buzz";}
+            if(i%7==0){output +="Name";}
             if(output=="") {output =String.valueOf(i);}
             System.out.println(output);
         }
